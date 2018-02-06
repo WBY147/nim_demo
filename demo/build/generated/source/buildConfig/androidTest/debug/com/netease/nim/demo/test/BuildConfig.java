8ecef49267e1c5536bbfa31ec150c596bd4f24a2
androidHarlan <linhuachon@163.com>
2018-02-06 11:36:32 +0800

    测试网易云

---
+++ demo/build/generated/source/buildConfig/androidTest/debug/com/netease/nim/demo/test/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.netease.nim.demo.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.netease.nim.demo.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 44;
+  public static final String VERSION_NAME = "4.7.3";
+  // Fields from default config.
+  public static final String BUILD_DATE = "2018-2-6 11:18:02";
+  public static final String GIT_REVISION = "";
+}
